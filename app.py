19b93871b16aec8f2991a16f09b64e08aa68be94
theriley106 <christopherlambert106@gmail.com>
2019-11-29 15:01:59 -0500

    Added long lat serach

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ try:
 except:
 	HERE_APP_ID = os.environ['HERE_APP_ID']
 	HERE_APP_CODE = os.environ['HERE_APP_CODE']
+from math import radians, cos, sin, asin, sqrt
 
 HERE_URL = "https://geocoder.api.here.com/6.2/geocode.json?searchtext={0}&gen=9&app_id={1}&app_code={2}"
 
@@ -18,12 +19,28 @@ app = Flask(__name__, static_url_path='/static')
 
 url = "https://devpost.com/hackathons?page={0}"
 
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
 def gen_geocode_url(address):
 	return HERE_URL.format(address, HERE_APP_ID, HERE_APP_CODE)
 
 def get_url(url):
 	res = requests.get(url)
-	return res.text
+	return res
 
 def is_time_between(begin_time, end_time, check_time=None):
     # If check time is not given, default to current UTC time
@@ -47,7 +64,6 @@ def fix_unicode(string1):
 def extract_datetime_from_date_range_string(dateRange):
 	dateRange = fix_unicode(dateRange)
 	# This means it's an actual range instead of a date
-	print(str(dateRange))
 	yearVal = str(dateRange)[::-1].partition(",")[0][::-1]
 	if "-" in str(dateRange):
 		range1 = "{}, {}".format(dateRange.partition(" - ")[0], yearVal)
@@ -70,41 +86,68 @@ def is_ongoing(dateRange):
 	return is_time_between(a, b)
 
 def get_address_from_hackathon(hackathonURL):
-	get_url(hackathonURL)
-
+	page = bs4.BeautifulSoup(get_url(hackathonURL).text, 'lxml')
+	address = str(page.select(".location")[0]).partition('https://maps.google.com/?q=')[2].partition('"')[0]
+	geocodeResponse = get_url(gen_geocode_url(address))
+	return geocodeResponse.json()['Response']['View'][0]['Result'][0]['Location']['NavigationPosition'][0]
 
 def pull_devpost():
 	toCheck = []
 	for page in [1,2,3]:
 		res = requests.get(url.format(page))
 		page = bs4.BeautifulSoup(res.text, 'lxml')
-		listOfHackathons = page.select(".clearfix")
-
+		listOfHackathons = page.select("a.clearfix")
 		for i, val in enumerate(listOfHackathons):
 			dateRange = val.select(".date-range")
 			# This means there is a date range present
 			if len(dateRange) != 0:
+				try:
+					title = val.select(".title")[0].getText().strip()
+				except:
+					raw_input(val)
 				ongoing = is_ongoing(dateRange[0].getText())
 				if ongoing == True:
 					hackathonURL = str(val).partition(' href="')[2].partition('"')[0]
 					if len(hackathonURL) > 0:
-						toCheck.append(hackathonURL)
+						toCheck.append((hackathonURL, title))
 				if ongoing == False and i == len(listOfHackathons) - 1:
 					return toCheck
 	return toCheck
 
 def get_nearby_hackathon(longitude, latitude):
+	results = []
 	for val in pull_devpost():
-	return
+		url, title = val
+		try:
+			result = get_address_from_hackathon(url)
+			lat1 = result["Latitude"]
+			lat2 = latitude
+			long1 = result["Longitude"]
+			long2 = longitude
+			info = {"title": title}
+			info["distance"] = haversine(long1, lat1, long2, lat2)
+			results.append(info)
+
+		except Exception as exp:
+			# print("ERROR PULLING {}".format(val))
+			# print(exp)
+			pass
+	return sorted(results, key=lambda k: k['distance'])
 
 @app.route('/getByLongLat', methods=['GET'])
 def pullFromLongLat():
 	longitude = request.args.get('long', None)
 	latitude = request.args.get('lat', None)
+	print longitude
+	print latitude
 	if latitude == None or longitude == None:
 		return "None"
-	return render_template("index1.html")
+	longitude = round(float(longitude), 7)
+	latitude = round(float(latitude), 7)
+	return get_nearby_hackathon(longitude, latitude)[0]['title']
+	# return render_template("index1.html")
 
 if __name__ == '__main__':
-	print pull_devpost()
-	# app.run(host='127.0.0.1', port=5000)
+	#print pull_devpost()
+	#print get_nearby_hackathon(34.7189911, -82.306362)
+	app.run(host='127.0.0.1', port=5000)
